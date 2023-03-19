0d2882821fd794371ea2425e1d5799b936fdebe0
nabilsaragih <nabilsaragih33@gmail.com>
2023-03-19 13:22:12 +0800

    Added secant.py

---
+++ Python/secant.py
@@ -0,0 +1,29 @@
+def f(x):
+    return (x ** 3 + x + 1) / (x ** 4 + 1)
+
+def secant(x0, x1, e):
+    iterCount = 1
+
+    while True:
+        fx0 = f(x0)
+        fx1 = f(x1)
+        x2 = x1 - ((fx1 * (x1 - x0)) / (fx1 - fx0))
+        fx2 = f(x2)
+
+        print(f"{iterCount} {format(x0, '.5f')} {format(x1, '.5f')} {format(x2, '.5f')} {format(fx0, '.5f')} {format(fx1, '.5f')} {format(fx2, '.5f')}")
+
+        if abs(fx1) <= e:
+            break
+
+        x0 = x1
+        x1 = x2
+
+        iterCount += 1
+
+    print(f"Akar dari persamaan adalah {format(x1, '.5f')}")
+
+x0 = float(input("Masukkan nilai x0: "))
+x1 = float(input("Masukkan nilai x1: "))
+e = float(input("Masukkan nilai error: "))
+
+secant(x0, x1, e)
